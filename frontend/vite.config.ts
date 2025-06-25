182951563802bce6552929245ba0568871d4be94
Matús Mandrák <matusmandrak@matuss-macbook-air.home>
2025-06-25 11:31:08 +0200

    Fix: Configure Vite for multi-page application

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,20 @@
+/// <reference types="node" />
+
+// @ts-nocheck
+
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        login: resolve(__dirname, 'login.html'),
+        signup: resolve(__dirname, 'signup.html'),
+        dashboard: resolve(__dirname, 'dashboard.html'),
+      },
+    },
+  },
 }) 
