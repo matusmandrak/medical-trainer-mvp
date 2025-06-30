0c2c150951a96bccf7373bd4d9236abdd74c0418
Matús Mandrák <matusmandrak@matuss-macbook-air.home>
2025-06-30 17:58:34 +0200

    new style for the homepage

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -15,6 +15,7 @@ export default defineConfig({
         trainer: resolve(__dirname, 'trainer.html'),
         dashboard: resolve(__dirname, 'dashboard.html'),
         scenarios: resolve(__dirname, 'scenarios.html'),
+        about_us: resolve(__dirname, 'about.html'),
       },
     },
   },
